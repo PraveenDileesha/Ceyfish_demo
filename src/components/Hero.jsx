112751ed87f1f3f3404db8244ba8dfdc53813be8
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2025-09-25 23:31:33 +0530

    Updated responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,23 @@
-import { HiOutlineMail } from "react-icons/hi";
 import { IoIosCall } from "react-icons/io";
-import bg_primary from "../assets/bg-primary.png";
 import fish from "../assets/fish.png";
 import logo2 from "../assets/logos/brc.png";
 import logo4 from "../assets/logos/dolphin safe gg.png";
 import logo3 from "../assets/logos/fots.png";
 import logo1 from "../assets/logos/haccp.png";
-import logo from "/logo.png";
+import footerLogo from "../assets/logos/UH.png";
 import EmailButton from "./EmailButton";
+import logo from "/logo.png";
 
 const Hero = () => {
   return (
-    <div
-      className="relative min-h-screen text-white"
-      style={{ backgroundImage: `url(${bg_primary})` }}
-    >
-      {/* Main site content with fade-in */}
-      <div className="flex flex-col lg:flex-row min-h-screen relative">
+    <div className="flex flex-col relative min-h-screen text-white bg-[url('/bg-primary.png')]">
+      <header className="fixed top-0 left-0 w-full bg-transparent px-6 sm:px-10 md:px-16 py-4 z-50">
+        <img src={logo} alt="fish" className="object-contain w-20 md:w-28" />
+      </header>
+
+      <main className="flex flex-col lg:flex-row min-h-screen relative">
         {/* Left Section (Text) */}
         <div className="flex flex-col justify-center px-6 sm:px-10 md:px-16 w-full lg:w-2/3">
-          <div className="mb-12">
-            <img src={logo} alt="fish" className="object-contain w-28" />
-          </div>
-
           <div className="border-l-4 border-secondary pl-4">
             <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight font-primary uppercase">
               Our Next <span className="text-secondary">Expedition</span>
@@ -41,25 +36,19 @@ const Hero = () => {
           </div>
 
           {/* Contact Section */}
-          <div className="mt-16 ml-14 font-secondary -rotate-4 uppercase">
-            <p className="text-bright-ocean-blue font-semibold text-3xl leading-0">
-              Questions or orders
-              <span className="text-5xl leading-0 font-accent">?</span>
+          <div className="mt-16 md:ml-14 font-body uppercase font-semibold">
+            <p className="text-bright-ocean-blue text-xl md:text-3xl">
+              Questions or orders ?
             </p>
-            <p className="text-2xl tracking-wider">
-              Get in touch with us today
-              <span className="text-6xl font-accent">!</span>
-            </p>
+            <p className="text-xl">Get in touch with us today !</p>
           </div>
 
           <div className="flex flex-col sm:flex-row sm:space-x-8 mt-10 sm:mt-16 lg:mt-24 ml-0 sm:ml-5 font-body gap-6 sm:gap-0">
-            <div className="btn-hero bg-secondary hover:border-btn-dark flex items-center">
+            <div className="btn-hero bg-secondary hover:border-btn-dark flex items-center justify-center">
               <span>
                 <IoIosCall className="btn-hero-icon text-deep-navy" />
               </span>
-              <p className="btn-hero-text text-deep-navy tracking-wide">
-                Call Our Team
-              </p>
+              <p className="btn-hero-text text-deep-navy">Call Our Team</p>
             </div>
 
             {/* Email Button with Dropdown */}
@@ -68,8 +57,8 @@ const Hero = () => {
         </div>
 
         {/* Right Section (Logos + Legal Text) */}
-        <div className="flex flex-col w-full lg:w-1/3 justify-center items-center px-6 sm:px-10 mt-10 lg:mt-0">
-          <div className="flex flex-wrap items-center justify-center gap-4 sm:gap-6">
+        <div className="flex flex-col w-full lg:w-1/3 justify-center items-center sm:px-10 mt-10 lg:mt-0">
+          <div className="flex flex-wrap items-center justify-center gap-3">
             <img
               src={logo1}
               alt="logo1"
@@ -92,9 +81,9 @@ const Hero = () => {
             />
           </div>
 
-          <div className="border-t border-secondary w-11/12 my-6"></div>
+          <div className="border-t border-secondary w-full md:w-11/12 my-6"></div>
 
-          <div className="text-sm space-y-4 text-center font-body font-medium">
+          <div className="text-xs md:text-sm space-y-4 text-center font-body font-medium px-4">
             <p>Licensed under the Safe Food for Canadians Regulations</p>
             <p>Registered with the U.S. Food and Drug Administration</p>
             <p>Approved for exports to the European Union</p>
@@ -106,19 +95,27 @@ const Hero = () => {
         </div>
 
         {/* Middle Fish */}
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none hidden lg:block">
+        <div className="absolute top-[280px] md:top-1/2 md:left-1/2 transform translate-x-44 md:-translate-x-1/2 md:-translate-y-1/2 pointer-events-none md:block">
           <img
             src={fish}
             alt="fish"
-            className="object-contain h-40 sm:h-60 md:h-72 lg:h-screen ml-[13%]"
+            className="object-contain md:h-72 lg:h-screen ml-[13%]"
           />
         </div>
+      </main>
 
-        {/* Copyright */}
-        <div className="absolute bottom-2 right-2 text-[10px] sm:text-xs text-center px-2">
-          © Ceyfish Co. is a trademark of Union Harvest Inc, Canada.
+      <footer className="fixed bottom-0 left-0 w-full flex items-center justify-between px-6 text-[10px] sm:text-xs font-body font-medium z-50">
+        <div className="flex items-center m-3">
+          <img
+            src={footerLogo}
+            alt="Footer Logo"
+            className="object-contain w-32 sm:w-36"
+          />
         </div>
-      </div>
+        <p className="text-right">
+          © Ceyfish Co. is a trademark of Union Harvest Inc, Canada.
+        </p>
+      </footer>
     </div>
   );
 };
