37fef0ed6ce2c544f7b6f73549dc0540adf186e2
PraveenDileesha <dileeshapraveen@gmail.com>
2025-10-21 21:11:50 +0530

    UI bug fixing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,17 +10,13 @@ import { HiOutlineMail } from "react-icons/hi";
 
 const Layout = () => {
   return (
-    <div className="relative min-h-screen w-full overflow-hidden">
-      {/* Fixed Background Container */}
-      <div className="fixed inset-0 -z-20 w-full h-full">
-        {/* Background Image */}
-        <div className="absolute inset-0 bg-[url('/bg-primary.png')] bg-cover bg-center" />
-        
-        {/* Fish Image - Fixed Position */}
+    <div className="relative min-h-screen w-full flex flex-col">
+      {/* Background Layer */}
+      <div className="fixed inset-0 -z-20 w-full h-full bg-[url('/bg-primary.png')] bg-cover bg-center">
         <img
           src={fish}
           alt="fish"
-          className="absolute top-0 w-full h-full object-contain
+          className="fixed top-0 w-full h-full object-contain
                              scale-150 -translate-y-10 md:translate-y-46 lg:translate-y-23 [@media(min-width:2400px)]:translate-y-[200px]
                               [@media(min-width:344px)_and_(max-width:400px)_and_(min-height:882px)]:-translate-y-20  
                              translate-x-3 lg:translate-x-24
@@ -28,18 +24,16 @@ const Layout = () => {
         />
 
         {/* Background Gradient Overlay */}
-        <div className="absolute bottom-0 left-0 w-full h-full md:h-full lg:h-[80%] bg-gradient-to-t from-black/85 via-black/60 to-transparent md:from-black/70 md:via-black/50 lg:from-black/70 lg:via-black/70 xl:from-black/60 xl:via-black/40" />
+        <div className="fixed bottom-0 left-0 w-full h-full md:h-full lg:h-[80%] bg-gradient-to-t from-black/85 via-black/60 to-transparent md:from-black/70 md:via-black/50 lg:from-black/70 lg:via-black/70 xl:from-black/60 xl:via-black/40" />
       </div>
 
-      {/* Content Container */}
-      <div className="relative z-10 min-h-screen flex flex-col">
-        {/* HEADER */}
-        <header className="w-full flex p-4">
-          <img src={logo} alt="Logo" className="object-contain w-15 md:w-20" />
-        </header>
+      {/* HEADER */}
+      <header className="relative z-10 w-full flex p-4">
+        <img src={logo} alt="Logo" className="object-contain w-15 md:w-20" />
+      </header>
 
-        {/* MAIN CONTENT */}
-        <main className="flex-grow flex flex-col justify-center px-3 py-8">
+      {/* MAIN CONTENT */}
+      <main className="relative z-10 flex-grow flex flex-col justify-center px-3 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-8 md:gap-12 text-white w-full space-y-10 md:space-y-14">
           {/* LEFT Section */}
           <div className="flex flex-col space-y-10 sm:space-y-14 lg:space-y-0">
@@ -122,20 +116,19 @@ const Layout = () => {
             </div>
           </div>
         </div>
-        </main>
+      </main>
 
-        {/* FOOTER */}
-        <footer className="w-full flex flex-col sm:flex-row items-center justify-between p-4 text-white">
-          <img
-            src={footerLogo}
-            alt="Footer Logo"
-            className="object-contain w-28 md:w-36 mb-2 sm:mb-0"
-          />
-          <p className="text-[8px] md:text-xs mt-2 sm:mt-0 sm:text-right text-center">
-            © Ceyfish Co. is a trademark of Union Harvest Inc, Canada.
-          </p>
-        </footer>
-      </div>
+      {/* FOOTER */}
+      <footer className="relative z-10 w-full flex flex-col sm:flex-row items-center justify-between p-4 text-white">
+        <img
+          src={footerLogo}
+          alt="Footer Logo"
+          className="object-contain w-28 md:w-36 mb-2 sm:mb-0"
+        />
+        <p className="text-[8px] md:text-xs mt-2 sm:mt-0 sm:text-right text-center">
+          © Ceyfish Co. is a trademark of Union Harvest Inc, Canada.
+        </p>
+      </footer>
     </div>
   );
 };
